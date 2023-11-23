65eaa2d108e91f9835ef447b0956534f8bf78038
ItsTopix <adriankroker2005@gmail.com>
2023-11-23 11:55:12 +0100

    remove scripts

--- docs/mkdocs/start_mkdocs.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-echo C:\Users\d22krokerad\AppData\Roaming\python\python310\scripts\mkdocs serve
-start %APPDATA%\python\python310\scripts\mkdocs serve
--- docs/mkdocs/start_mkdocs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/home/itstopix/Schreibtisch/Python/mkdocs/venv/bin/python -m mkdocs serve
