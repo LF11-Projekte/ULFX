e675853e24012a93ca48f96f44c06dab825ccb59
ItsTopix <adriankroker2005@gmail.com>
2024-01-26 17:47:30 +0100

    Express hello world

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World2")
+import express, { Express, Request, Response } from "express"
+
+const app: Express = express();
+const port = 8000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log("Express is listening on port ", port);
+})
